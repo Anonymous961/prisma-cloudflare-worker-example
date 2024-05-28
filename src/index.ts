7718ca73d4de61d924fa0245f0564a91072b94ee
Anil Kumar Behera <ark845612@gmail.com>
2024-05-28 19:00:22 +0530

    using with accelerate

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,43 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+export interface Env {
+	DATABASE_URL: string
+}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		try {
+			const prisma = new PrismaClient({ datasourceUrl: env.DATABASE_URL }).$extends(withAccelerate())
+
+			await prisma.user.create({
+				data: {
+					email: "test1@gmail.com",
+					name: "test1"
+				}
+			})
+			const users = await prisma.user.findMany()
+			console.log(users);
+			const result = JSON.stringify(users)
+			return new Response(result, {
+				status: 200,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+
+		} catch (error) {
+			console.error('Error:', error);
+			return new Response(JSON.stringify({ error: error.message }), {
+				status: 500,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
 	},
-};
+}
